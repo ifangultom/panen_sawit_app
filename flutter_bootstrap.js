5840802d4643cee1578696b6056be803e31f32e7
ifangultom <ifangultom8@gmail.com>
2026-06-06 05:02:13 +0700

    Updates

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"425cfb54d01a9472b3e81d9e76fd63a4a44cfb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4119158623" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2247306845" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
